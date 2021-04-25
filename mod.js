07cef6945ba7309b412e08985650d9622b1d1275
timreichen <tim.reichen@gmail.com>
2021-04-25 10:03:55 +0200

    add missing stars

--- mod.js
+++ mod.js
@@ -1,23 +1,32 @@
 async function handleRequest(request) {
-  const denoStars =
+  const denoStars = Number(
     (await fetch("https://api.github.com/repos/denoland/deno").then((data) =>
       data.json()
-    )).stargazers_count;
-  const nodeStars =
+    )).stargazers_count,
+  );
+  const nodeStars = Number(
     (await fetch("https://api.github.com/repos/nodejs/node").then((data) =>
       data.json()
-    )).stargazers_count;
+    )).stargazers_count,
+  );
+
+  let differenceText;
+  if (nodeStars > denoStars) {
+    differenceText = `${nodeStars - denoStars} ⭐️ missing`;
+  } else {
+    differenceText = "We have done it!";
+  }
 
   return new Response(
-    `<body
-      align="center"
-      style="font-family: Avenir, Helvetica, Arial, sans-serif; font-size: 1.5rem;"
-    >
+    `<body align="center" style="font-family: Avenir, Helvetica, Arial, sans-serif; font-size: 1.5rem;">
       <h1>Deno</h1>
-      <span>${denoStars}⭐️</span>
+      <span>${denoStars} ⭐️</span>
 
       <h1>Node</h1>
-        <span>${nodeStars}⭐️</span>
+      <span>${nodeStars} ⭐️</span>
+
+      <span>${text}</span>
+      
       </body>`,
     {
       headers: {
