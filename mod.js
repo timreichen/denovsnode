f053c434af6ea426d6aff0baa82b9c465bc33c62
timreichen <tim.reichen@gmail.com>
2021-04-25 10:00:23 +0200

    add stars count

--- mod.js
+++ mod.js
@@ -1,49 +1,24 @@
-function handleRequest(request) {
-  const { pathname } = new URL(request.url);
-
-  if (pathname.startsWith("/html")) {
-    const html = `<html>
-      <p><b>Message:</b> Hello from Deno Deploy.</p>
-      </html>`;
-
-    return new Response(html, {
-      headers: {
-        // The interpretation of the body of the response by the client depends
-        // on the 'content-type' header.
-        // The "text/html" part implies to the client that the content is HTML
-        // and the "charset=UTF-8" part implies to the client that the content
-        // is encoded using UTF-8.
-        "content-type": "text/html; charset=UTF-8",
-      },
-    });
-  }
-
-  if (pathname.startsWith("/json")) {
-    // Use stringify function to convert javascript object to JSON string.
-    const json = JSON.stringify({
-      message: "Hello from Deno Deploy",
-    });
-
-    return new Response(json, {
-      headers: {
-        "content-type": "application/json; charset=UTF-8",
-      },
-    });
-  }
+async function handleRequest(request) {
+  const denoStars =
+    (await fetch("https://api.github.com/repos/denoland/deno").then((data) =>
+      data.json()
+    )).stargazers_count;
+  const nodeStars =
+    (await fetch("https://api.github.com/repos/nodejs/node").then((data) =>
+      data.json()
+    )).stargazers_count;
 
   return new Response(
     `<body
       align="center"
       style="font-family: Avenir, Helvetica, Arial, sans-serif; font-size: 1.5rem;"
     >
-      <h1>Return JSON and/or HTML Example</h1>
-      <p>
-        <a href="/html">/html</a> - responds with HTML to the request.
-      </p>
-      <p>
-        <a href="/json">/json</a> - responds with JSON to the request.
-      </p>
-    </body>`,
+      <h1>Deno</h1>
+      <span>${denoStars}⭐️</span>
+
+      <h1>Node</h1>
+        <span>${nodeStars}⭐️</span>
+      </body>`,
     {
       headers: {
         "content-type": "text/html; charset=UTF-8",
